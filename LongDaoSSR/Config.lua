f9fd9373d908d87ea7722f183a92ed0c8872f2d0
Naced <869521569@qq.com>
2022-09-29 22:25:07 +0800

    base

---
+++ LongDaoSSR/Config.lua
@@ -0,0 +1,8 @@
+return {
+	Title = "龙岛忠仆SSR",
+	Version = 1,
+	BackendPlugins = {"LongDaoSSR.dll"}, 
+	Author = "Naced",
+	Description = "龙岛忠仆加强版 https://github.com/NacedWang/Taiwu_LongDaoSSR_new",
+	Cover = "logo.png"
+}
